03b2db41a801a9791186ac4f103238fd2b44a945
Harper Nettesheim <harpernettesheim@gmail.com>
2020-10-02 11:54:43 -0500

    MVP for the sprint

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    data = {}
+    for i in range(len(weights)):
+        data[weights[i]] = i
+    for i in range(len(weights)):
+        diff = limit-weights[i]
+        if diff in data:
+            return (max(i, data[diff]), min(i, data[diff]))
+
     return None
